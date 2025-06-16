1aee83f4a43fa41737efcea62ce1669874d60e0b
Devanshu1328 <Devanshu@c0de4dev.com>
2025-06-16 11:45:16 +0100

    Add project files.

---
+++ MyM365Agent2/Common/Interface/IApprovalConnector.cs
@@ -0,0 +1,10 @@
+﻿using MyM365Agent2.Common.Models;
+
+namespace MyM365Agent2.Common.Interface
+{
+    public interface IApprovalConnector
+    {
+        Task<IEnumerable<ApprovalItem>> GetPendingApprovalsAsync(string managerId);
+        Task<bool> UpdateApprovalStatusAsync(string id, string managerId, int newStatus);
+    }
+}
